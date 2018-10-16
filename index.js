38d1180a03331d671986dc0a90a0f92b4505bee3
Evgen Loshkarev <e.loshkarev@englishdom.com>
2018-10-16 17:40:17 +0300

    path fixes

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ module.exports = async function(copyList, pathFrom, pathTo) {
   var pathFrom = pathFrom || 'node_modules';
   var filesCount = 0;
 
-  if (!fs.existsSync(path.resolve(__dirname, pathTo))) {
-    fs.mkdir(path.resolve(__dirname, pathTo));
+  if (!fs.existsSync(path.resolve(pathTo))) {
+    fs.mkdir(path.resolve(pathTo));
 
   } else {
     var deleteFolderRecursive = function(pathFind) {
       if( fs.existsSync(pathFind)) {
-        if (!pathFind) pathFind = path.resolve(__dirname, pathTo);
+        if (!pathFind) pathFind = path.resolve(pathTo);
 
         fs.readdirSync(pathFind).forEach(function(file){
           pathFind = pathFind + '/' + file;
@@ -34,10 +34,10 @@ module.exports = async function(copyList, pathFrom, pathTo) {
   }
 
   for (var i = 0; i < copyList.length; i++) {
-    var filePath = path.resolve(__dirname, pathFrom + '/' + copyList[i] + '.js');
+    var filePath = path.resolve(pathFrom + '/' + copyList[i] + '.js');
     var file = copyList[i];
 
-    fs.copyFileSync(filePath, path.resolve(__dirname, pathTo + file + '.js'), {
+    fs.copyFileSync(filePath, path.resolve(pathTo + '/' + file + '.js'), {
       process: function(contents) {
         filesCount += 1;
       }
